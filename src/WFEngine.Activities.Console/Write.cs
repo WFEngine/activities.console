95ee52a0a15669e8cf7f925315bbb48cee09e6a7
Ömer Faruk Şahin <faruk_thecno@hotmail.com>
2020-12-09 22:19:28 +0300

    WriteLine and Write Functions

---
+++ src/WFEngine.Activities.Console/Write.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Linq;
+using WFEngine.Activities.Core;
+using WFEngine.Activities.Core.Model;
+
+namespace WFEngine.Activities.Console
+{
+    public class Write :WFActivity
+    {
+        public override string CompileActivity()
+        {
+            WFArgument message = Arguments.FirstOrDefault(x => x.Name == "Message");
+            if (message == null)
+                throw new ArgumentNullException(nameof(message));
+            string code = "";
+            if (message.IsVariable)
+                code = $"System.Console.Write({message.Value});";
+            else
+            {
+                if (message.ArgumentType != typeof(string).FullName)
+                    throw new Exception();
+                else
+                {
+                    var value = (Newtonsoft.Json.Linq.JArray)message.Value;
+                    code = $"System.Console.Write(\"{value.First.ToString()}\");";
+                }
+            }
+            return code;
+        }
+    }
+}
---
+++ src/WFEngine.Activities.Console/WriteLine.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Linq;
+using WFEngine.Activities.Core;
+using WFEngine.Activities.Core.Model;
+
+namespace WFEngine.Activities.Console
+{
+    public class WriteLine : WFActivity
+    {
+        public override string CompileActivity()
+        {
+            WFArgument message = Arguments.FirstOrDefault(x => x.Name == "Message");
+            if (message == null)
+                throw new ArgumentNullException(nameof(message));
+            string code = "";
+            if (message.IsVariable)
+                code = $"System.Console.WriteLine({message.Value});";
+            else
+            {
+                if (message.ArgumentType != typeof(string).FullName)
+                    throw new Exception();
+                else
+                {
+                    var value = (Newtonsoft.Json.Linq.JArray)message.Value;
+                    code = $"System.Console.WriteLine(\"{value.First.ToString()}\");";
+                }
+            }
+            return code;
+        }
+    }
+}
